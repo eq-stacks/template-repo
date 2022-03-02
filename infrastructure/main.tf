e91ab605edc79d933133fe3617c475393aa9207d
Mark Henderson <mark@mrh.io>
2022-03-02 15:04:08 -0500

    feat: add host VPC and subnetwork binding

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,7 @@ variable "org_id" {}
 variable "billing_account" {}
 variable "project_owners" {}
 variable "project_host_vpc" {}
+variable "subnetwork_id" {}
 
 terraform {
   required_providers {
@@ -46,4 +47,47 @@ resource "google_project_iam_binding" "target_project_owners" {
 resource "google_compute_shared_vpc_service_project" "service_project" {
   host_project    = var.project_host_vpc
   service_project = google_project.target_project.project_id
+}
+
+resource "google_project_service" "container" {
+  project = google_project.target_project.id
+  service = "container.googleapis.com"
+
+  disable_dependent_services = true
+  depends_on = [
+    google_project.target_project
+  ]
+}
+
+resource "google_project_iam_binding" "host_project" {
+  project = var.project_host_vpc
+  role    = "roles/container.hostServiceAgentUser"
+  members = [
+    "serviceAccount:service-${google_project.target_project.number}@container-engine-robot.iam.gserviceaccount.com"
+  ]
+  depends_on = [
+    google_project_service.container
+  ]
+}
+
+data "google_compute_network" "host_vpc" {
+  name    = "host-vpc"
+  project = var.project_host_vpc
+}
+
+data "google_compute_subnetwork" "us_east4-2" {
+  region  = "us-east4"
+  name    = var.subnetwork_id
+  project = var.project_host_vpc
+}
+
+resource "google_compute_subnetwork_iam_binding" "subnetwork" {
+  project    = var.project_host_vpc
+  region     = "us-east4"
+  subnetwork = data.google_compute_subnetwork.id
+  role       = "roles/compute.networkUser"
+  members = [
+    "serviceAccount:service-${google_project.target_project.number}@container-engine-robot.iam.gserviceaccount.com",
+    "serviceAccount:${google_project.target_project.number}@cloudservices.gserviceaccount.com"
+  ]
 }
